c391641536b8530e0b29c6dadaad20f6b0e72709
Josh McDannel <mcdannel@gmail.com>
2015-11-04 14:44:16 -0700

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngRiverDeckApp
+ * @description
+ * # ngRiverDeckApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngRiverDeckApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngRiverDeckApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngRiverDeckApp
+ */
+angular.module('ngRiverDeckApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
